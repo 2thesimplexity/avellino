c4aa0b58e6613ae90004e806e5f27b972b0fcdea
Triet Tran <2thesimplexity@gmail.com>
2020-10-29 20:41:27 -0700

    finalized

--- avellino/forms.py
+++ avellino/forms.py
@@ -4,17 +4,15 @@ from .models import Post
 
 class PostForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', '')
+        super(PostForm, self).__init__(*args, **kwargs)
+    
     class Meta:
         model = Post
         fields= "__all__"
-       
-
-        
 
 
-'''remove the stupid colon followed by label that's automatically come with Django forms'''
-'''It worked yesterday, why doesn't it work today???'''
 
-def __init__(self, *args, **kwargs):
-        kwargs.setdefault('label_suffix', '')
-        super(PostForm, self).__init__(*args, **kwargs)
+
